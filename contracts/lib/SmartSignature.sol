a59d690d7a418cc63f417c36dcca587b156e73ea
xiaodao <lychees67@gmail.com>
2018-06-26 19:38:00 -0700

    Refact: standalone contract.

--- contracts/lib/SmartSignature.sol
+++ contracts/lib/SmartSignature.sol
@@ -1,8 +1,11 @@
 pragma solidity ^0.4.23;
 
-import "ERC721.sol";
+import "./ERC721.sol";
+import "./AddressUtils.sol";
 
 contract SmartSignature is ERC721{
+    using AddressUtils for address;
+        
     function SmartSignature() public {
         owner = msg.sender;
         admins[owner] = true;    
